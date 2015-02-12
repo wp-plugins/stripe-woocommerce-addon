425d53cb22fb8a0150252959f853016c5ab2607a
nazrulhassanmca <nazrulhassanmca@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-12 23:02:50 +0000

    adding version 1 of stripe-woocommerce-addon
    git-svn-id: https://plugins.svn.wordpress.org/stripe-woocommerce-addon/trunk@1088533 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,43 @@
+==== Stripe Woocommerce Addon ====
+Contributors: Syed Nazrul Hassan
+Tags: woocommerce, stripe, woocommerce addon stripe, stripe for woocommerce,stripe for wordpress
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=nazrulhassanmca@gmail.com&item_name=Donation+Stripe+Woocommerce+Addon
+Requires at least: 3.8.0
+Tested up to: 4.1.0 & Woocommerce 2.3
+Stable tag: 1.0
+License: GPLv3
+
+== Description ==
+
+This plugin acts as an addon for woocommerce to implement a payment gateway for stripe for accepting credit cards payments
+This plugin uses Stripe API version  2015-01-26 to create tokens and charge credit cards
+
+== Screenshots ==
+
+1. Screenshot 1 - Api Key Location 
+2. Screenshot 2 - Admin Settings of Addon
+3. Screenshot 3 - Checkout Page Form
+
+== Installation ==
+
+1) Upload 'stripe-woo-commerce' folder to the '/wp-content/plugins/' directory
+2) Activate 'Stripe Woocommerce Addon' from wp plugin lists in admin area
+3) Plugin will appear in settings of woocommerce
+4) You can set the addon settings from wocommmerce->settings->Checkout->Stripe Settings for Cards
+5) You can check for Testing Card No Here https://stripe.com/docs/testing
+6) Integrated Stripe Libraries
+
+== Frequently Asked Questions ==
+1) You need to have woocoommerce plugin installed to make this plugin work
+2) You need to follow The Screeenshot 1 to obtain API keys from Stripe
+3) This plugin works on test & live api keys. 
+4) This plugin readily works on local.
+5) This plugin does nor requires SSL.
+6) This plugin does not store Card Details anywhere.
+7) This plugin comes packed with Official Stripe Libraries
+
+== Changelog ==
+This is first version no known errors found
+
+== Upgrade Notice == 
+This is first version no known notices yet
---
+++ stripe-woocommerce-addon.php
@@ -0,0 +1,235 @@
+<?php
+/*
+Plugin Name: Stripe Woocommerce Addon
+Plugin URI: https://nazrulhassan.wordpress.com/
+Description: Add a feature in wocommerce to pay via stripe. 
+Version: 1.0.0
+Author: Syed Nazrul Hassan
+Author URI: https://nazrulhassan.wordpress.com/
+*/
+
+
+function stripe_init()
+{
+
+function add_stripe_gateway_class( $methods ) 
+{
+	$methods[] = 'WC_Stripe_Gateway'; 
+	return $methods;
+}
+add_filter( 'woocommerce_payment_gateways', 'add_stripe_gateway_class' );
+
+if(class_exists('WC_Payment_Gateway'))
+{
+	class WC_Stripe_Gateway extends WC_Payment_Gateway 
+	{
+		
+		public function __construct()
+		{
+
+		$this->id               = 'stripe';
+		$this->icon             = apply_filters( 'woocommerce_stripe_icon', plugins_url( 'images/stripe.png' , __FILE__ ) );
+		$this->has_fields       = true;
+		$this->method_title     = 'Stripe Settings for Cards';		
+		$this->init_form_fields();
+		$this->init_settings();
+		$this->title               	 = $this->get_option( 'stripe_title' );
+		$this->stripe_testsecretkey      = $this->get_option( 'stripe_testsecretkey' );
+		$this->stripe_livesecretkey      = $this->get_option( 'stripe_livesecretkey' );
+		$this->stripe_sandbox            = $this->get_option( 'stripe_sandbox' ); 
+
+		define("STRIPE_SANDBOX", ($this->stripe_sandbox=='yes'? true : false));
+
+		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+
+		}
+
+		public function admin_options()
+		{
+		?>
+		<h3><?php _e( 'Stripe addon for Woocommerce', 'woocommerce' ); ?></h3>
+		<p><?php  _e( 'Stripe is a company that provides a way for individuals and businesses to accept payments over the Internet.', 'woocommerce' ); ?></p>
+		<table class="form-table">
+		  <?php $this->generate_settings_html(); ?>
+		</table>
+		<?php
+		}
+
+		public function init_form_fields()
+		{
+		$this->form_fields = array(
+		'enabled' => array(
+		  'title' => __( 'Enable/Disable', 'woocommerce' ),
+		  'type' => 'checkbox',
+		  'label' => __( 'Enable Stripe', 'woocommerce' ),
+		  'default' => 'yes'
+		  ),
+		'stripe_title' => array(
+		  'title' => __( 'Title', 'woocommerce' ),
+		  'type' => 'text',
+		  'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce' ),
+		  'default' => __( 'Stripe', 'woocommerce' ),
+		  'desc_tip'      => true,
+		  ),
+		'stripe_testsecretkey' => array(
+		  'title' => __( 'Test Secret Key', 'woocommerce' ),
+		  'type' => 'text',
+		  'description' => __( 'This is the Secret Key found in API Keys in Account Dashboard.', 'woocommerce' ),
+		  'default' => '',
+		  'desc_tip'      => true,
+		  'placeholder' => 'Stripe Test Secret Key'
+		  ),
+		
+		'stripe_livesecretkey' => array(
+		  'title' => __( 'Live Secret Key', 'woocommerce' ),
+		  'type' => 'text',
+		  'description' => __( 'This is the Secret Key found in API Keys in Account Dashboard.', 'woocommerce' ),
+		  'default' => '',
+		  'desc_tip'      => true,
+		  'placeholder' => 'Stripe Live Secret Key'
+		  ),
+
+		
+		'stripe_sandbox' => array(
+		  'title'       => __( 'stripe sandbox', 'woocommerce' ),
+		  'type'        => 'checkbox',
+		  'label'       => __( 'Enable stripe sandbox', 'woocommerce' ),
+		  'default'     => 'no',
+		  'description' => __( 'If checked its in sanbox mode and if unchecked its in live mode', 'woocommerce' )
+		)
+		
+	  );
+  		}
+
+		public function payment_fields()
+		{			
+	?>
+		<table>
+		    <tr>
+		    	<td><label class="" for="stripe_cardno"><?php echo __( 'Card No.', 'woocommerce') ?></label></td>
+			<td><input type="text" name="stripe_cardno" class="input-text" /></td>
+		    </tr>
+		    <tr>
+		    	<td><label class="" for="stripe_expiration_date"><?php echo __( 'Expiration date', 'woocommerce') ?>.</label></td>
+			<td>
+			   <select name="stripe_expmonth" id="expmonth">
+			      <option value=""><?php _e( 'Month', 'woocommerce' ) ?></option>
+			      <option value='01'>01</option>
+			      <option value='02'>02</option>
+			      <option value='03'>03</option>
+			      <option value='04'>04</option>
+			      <option value='05'>05</option>
+			      <option value='06'>06</option>
+			      <option value='07'>07</option>
+			      <option value='08'>08</option>
+			      <option value='09'>09</option>
+			      <option value='10'>10</option>
+			      <option value='11'>11</option>
+			      <option value='12'>12</option>  
+			    </select>
+			    <select name="stripe_expyear" id="expyear">
+			      <option value=""><?php _e( 'Year', 'woocommerce' ) ?></option><?php
+			      $years = array();
+			      for ( $i = date( 'y' ); $i <= date( 'y' ) + 15; $i ++ ) {
+				printf( '<option value="20%u">20%u</option>', $i, $i );
+			      } ?>
+			    </select>
+			</td>
+		    </tr>
+		    <tr>
+		    	<td><label class="" for="stripe_cardcvv"><?php echo __( 'Card CVC', 'woocommerce') ?></label></td>
+			<td><input type="text" name="stripe_cardcvv" class="input-text" /></td>
+		    </tr>
+		</table>
+	        <?php  
+		} // end of public function payment_fields()
+
+		public function process_payment( $order_id )
+		{
+		global $error;
+		global $woocommerce;
+		$wc_order 	= new WC_Order( $order_id );
+		$grand_total 	= $wc_order->order_total;
+		$amount 	     = $grand_total * 100 ;
+
+		include(plugin_dir_path( __FILE__ )."lib/Stripe.php");
+	
+		if(STRIPE_SANDBOX == 'yes')
+		{ Stripe::setApiKey($this->stripe_testsecretkey);  }
+		else
+		{ Stripe::setApiKey($this->stripe_livesecretkey);   }
+		
+			
+		// create token for customer/buyer credit card
+
+		$token_id = Stripe_Token::create(array(
+			 				"card" => array( 
+			 						"number" 	     => $_POST['stripe_cardno'], 
+									"exp_month" 	=> $_POST['stripe_expmonth'], 
+									"exp_year" 	=> $_POST['stripe_expyear'], 
+									"cvc" 		=> $_POST['stripe_cardcvv'] 
+									) 
+				            	      )
+						);
+
+		// charge customer/buyer credit card based on card's unique token 
+	
+		try
+		{
+
+		$charge = Stripe_Charge::create(array( 
+							"amount" 	     => $amount, 
+							"currency" 	=> "USD", 
+							"card"		=> $token_id->id, 
+							"metadata" 	=> array("order_id" => $order_id) ,
+							"description"  => $wc_order->billing_email
+						     )
+						);
+		}
+		
+		catch (Exception $e) 
+		{	
+			
+			$body         = $e->getJsonBody(); 
+			$error = $body['error']['message'];
+ 			echo json_encode(array('message' => $error ));
+			
+		}
+
+		if($token_id->id !='')
+		{
+		  if ($charge->paid == true) 
+		  {
+			$epoch     = $charge->created;
+			$dt        = new DateTime("@$epoch"); 
+			$timestamp =  $dt->format('Y-m-d H:i:s e');
+			$chargeid  = $charge->id ; 
+
+		  	$wc_order->add_order_note( __( 'Stripe payment completed at. '.$timestamp.' with Charge ID = '.$chargeid , 'woocommerce' ) );
+			$wc_order->payment_complete();
+			return array (
+			  'result'   => 'success',
+			  'redirect' => $this->get_return_url( $wc_order ),
+			);
+		  }
+		  else
+		  {
+			$wc_order->add_order_note( __( 'Stripe payment failed.'.$error, 'woocommerce' ) );
+			$woocommerce->add_error( __( 'Sorry, Error.'.$error, 'woocommerce' ) );
+		  }
+		  
+		}
+		
+
+
+
+
+		} // end of function process_payment()
+
+	}  // end of class WC_Stripe_Gateway
+
+} // end of if class exist WC_Gateway
+
+}
+
+add_action( 'plugins_loaded', 'stripe_init' );
