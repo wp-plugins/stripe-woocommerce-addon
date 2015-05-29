af94389e6de79672c73549f274bbb5af2f37d794
nazrulhassanmca <nazrulhassanmca@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-29 14:53:16 +0000

    tagging version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/stripe-woocommerce-addon/trunk@1170202 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -6,10 +6,10 @@ Tags: woocommerce, stripe, woocommerce addon stripe, stripe for woocommerce,stri
 Author URI: https://nazrulhassan.wordpress.com/
 Author: nazrulhassanmca
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=nazrulhassan@ymail.com&item_name=Donation+Stripe+Woocommerce+Addon
-Requires at least: 4.0 
+Requires at least: 4.0  & WooCommerce 2.2+
 Tested up to: 4.2.2 & WooCommerce 2.3.9
-Stable tag: 1.0.1
-Version: 1.0.1
+Stable tag: 1.0.2
+Version: 1.0.2
 License: http://www.gnu.org/licenses/gpl-2.0.html
 
 
@@ -25,23 +25,22 @@ This plugin uses Stripe API version  **2015-04-07** to create tokens and charge 
 3. Prerequisite visualized on screenshots.
 4. Adds Charde Id and Charge time to Order Note.
 5. Can be customized easily.
-6. Bundles with Official Stripe® API Libraries.
+6. This plugin uses Bundles with <a href="https://github.com/stripe/stripe-php">Official Stripe® API Libraries</a> Version 1.18.0 to support PHP 5.2 Compatibility.
 7. Can work with test/sandbox mode of Stripe accounts for testing purpose.
 8. Uses Token method to charge Credit Cards rather sending sensitive card details to stripe directly as prescribed by Stripe.
-9. It does not needs SSL.
+9. This plugin requires SSL on merchant site as described <a href="https://stripe.com/help/ssl">here</a>.
 10. Single checkbox to put it in live/test mode.
 11. Single checkbox to put it in Authorize or Authorize & Capture.
 12. This plugin Support to accept card types.
-13. This plugin **does not store Credit Card Details**.
-14. This plugin Support refunds in woocommerce interface.
-
-15. This plugin currently supports Multiple Currency accepted one at a time Irrespective of store base currency See Screenshot No.2
+13. This plugin does not store **Credit Card Details**
+14. This plugin Support refunds **(Only in Cents)** in woocommerce interface. On full refund order state changes automatically to refunded(WooCommerce Feature).
+15. Upon refunds the items are not restocked automatically you need to use <a href="https://wordpress.org/plugins/woocommerce-auto-restore-stock/">this plugin</a> to restock automatically.
+16. This plugin currently supports Multiple Currency accepted one at a time Irrespective of store base currency See Screenshot No.2
 
 Also See
 
 	1. https://support.stripe.com/questions/which-currencies-does-stripe-support
 	
-
 
 
 == Screenshots ==
@@ -60,23 +59,45 @@ Also See
 3. Plugin will appear in settings of woocommerce
 4. You can set the addon settings from 
    wocommmerce -> settings -> Checkout -> Stripe Cards Settings
-5. You can check for Testing Card No Here https://stripe.com/docs/testing
+5. You can check for Testing Card No <a href="https://stripe.com/docs/testing" target="_blank" >Here</a> 
 6. Integrated Stripe Libraries
 
 == Frequently Asked Questions ==
 
 1. You need to have woocoommerce plugin installed to make this plugin work
-2. You need to follow The Screeenshot 1 to obtain API keys from Stripe https://dashboard.stripe.com/
+2. You need to follow The Screeenshot 1 to obtain API keys from Stripe <a href="https://dashboard.stripe.com/dashboard">Dashboard</a>
 3. This plugin works on test & live api keys.
 4. This plugin readily works on local.
-5. This plugin does nor requires SSL.
+5. This plugin requires SSL as per <a href="https://stripe.com/help/ssl">Here</a>
 6. This plugin does not store Card Details anywhere.
 7. This plugin comes packed with Official Stripe Libraries
 8. This plugin requires CURL
 9. This plugin does not support Pre Order or Subscriptions 
+10. Stripe & PCI compliance requires to use SSL always
+
 
 == Changelog ==
-This is first version no known errors found
 
+2015.05.25 - Version 1.0.2
+
+	1. Added sending billing email in meta for fraud tracking
+	2. Fixed Warnings & Notices when plugin run in WP debug mode set to true.
+	3. Added sending Woocommerce refund reason text in refund meta.
+	4. Performance improvement and other minor bug fixes.
+
+2015.05.25 - Version 1.0.1
+
+	1. Added Sending Shipping Address to stripe
+	2. Added Sending of Name , Zipcode to Stripe 
+	3. Added support of more metadata( WP user_id,Customer IP,Tax,Shipping etc)
+	4. Added support to accept card types
+	5. Added support for authorize or authorize & capture
+	6. Added performance improvement and bugfixes
+	7. Added support for refunds from WooCommerce Interface via Stripe API.
+	
+2015.02.12 - Version 1.0.0
+
+	1. First Release
+	
 == Upgrade Notice == 
 This is first version no known notices yet
--- stripe-woocommerce-addon.php
+++ stripe-woocommerce-addon.php
@@ -3,7 +3,7 @@
  * Plugin Name: Stripe WooCommerce Addon
  * Plugin URI: https://wordpress.org/plugins/stripe-woocommerce-addon/
  * Description: This plugin adds a payment option in WooCommerce for customers to pay with their Credit Cards Via Stripe.
- * Version: 1.0.0
+ * Version: 1.0.2
  * Author: Syed Nazrul Hassan
  * Author URI: https://nazrulhassan.wordpress.com/
  * License: GPL2
@@ -49,12 +49,22 @@ if(class_exists('WC_Payment_Gateway'))
 		$this->stripe_authorize_only     = $this->get_option( 'stripe_authorize_only' );
 		$this->stripe_cardtypes          = $this->get_option( 'stripe_cardtypes');
 		
-
-		define("STRIPE_SANDBOX"           , ($this->stripe_sandbox        =='yes'? true : false));
-		define("STRIPE_TRANSACTION_MODE"  , ($this->stripe_authorize_only =='yes'? false : true));
+		if(!defined("STRIPE_SANDBOX"))
+		{ define("STRIPE_SANDBOX"           , ($this->stripe_sandbox        =='yes'? true : false)); }
 		
-
-		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+		if(!defined("STRIPE_TRANSACTION_MODE"))
+		{ define("STRIPE_TRANSACTION_MODE"  , ($this->stripe_authorize_only =='yes'? false : true)); }
+		
+		if(STRIPE_SANDBOX == 'yes')
+		{ Stripe::setApiKey($this->stripe_testsecretkey);  }
+		else
+		{ Stripe::setApiKey($this->stripe_livesecretkey);  }
+		
+		if (is_admin()) 
+		{
+			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+		}
+		
 
 		}
 
@@ -113,7 +123,7 @@ if(class_exists('WC_Payment_Gateway'))
                   'options'      => array( 'USD'=>' United States Dollar','AED'=>'United Arab Emirates Dirham','AFN'=>' Afghan Afghani*','ALL'=>' Albanian Lek','AMD'=>' Armenian Dram','ANG'=>' Netherlands Antillean Gulden','AOA'=>' Angolan Kwanza*','ARS'=>' Argentine Peso*','AUD'=>' Australian Dollar','AWG'=>' Aruban Florin','AZN'=>' Azerbaijani Manat','BAM'=>' Bosnia & Herzegovina Convertible Mark','BBD'=>' Barbadian Dollar','BDT'=>' Bangladeshi Taka','BGN'=>' Bulgarian Lev','BIF'=>' Burundian Franc','BMD'=>' Bermudian Dollar','BND'=>' Brunei Dollar','BOB'=>' Bolivian Boliviano*','BRL'=>' Brazilian Real*','BSD'=>' Bahamian Dollar','BWP'=>' Botswana Pula','BZD'=>' Belize Dollar','CAD'=>' Canadian Dollar','CDF'=>' Congolese Franc','CHF'=>' Swiss Franc','CLP'=>' Chilean Peso*','CNY'=>' Chinese Renminbi Yuan','COP'=>' Colombian Peso*','CRC'=>' Costa Rican Colón*','CVE'=>' Cape Verdean Escudo*','CZK'=>' Czech Koruna*','DJF'=>' Djiboutian Franc*','DKK'=>' Danish Krone','DOP'=>' Dominican Peso','DZD'=>' Algerian Dinar','EEK'=>' Estonian Kroon*','EGP'=>' Egyptian Pound','ETB'=>' Ethiopian Birr','EUR'=>' Euro','FJD'=>' Fijian Dollar','FKP'=>' Falkland Islands Pound*','GBP'=>' British Pound','GEL'=>' Georgian Lari','GIP'=>' Gibraltar Pound','GMD'=>' Gambian Dalasi','GNF'=>' Guinean Franc*','GTQ'=>' Guatemalan Quetzal*','GYD'=>' Guyanese Dollar','HKD'=>' Hong Kong Dollar','HNL'=>' Honduran Lempira*','HRK'=>' Croatian Kuna','HTG'=>' Haitian Gourde','HUF'=>' Hungarian Forint*','IDR'=>' Indonesian Rupiah','ILS'=>' Israeli New Sheqel','INR'=>' Indian Rupee*','ISK'=>' Icelandic Króna','JMD'=>' Jamaican Dollar','JPY'=>' Japanese Yen','KES'=>' Kenyan Shilling','KGS'=>' Kyrgyzstani Som','KHR'=>' Cambodian Riel','KMF'=>' Comorian Franc','KRW'=>' South Korean Won','KYD'=>' Cayman Islands Dollar','KZT'=>' Kazakhstani Tenge','LAK'=>' Lao Kip*','LBP'=>' Lebanese Pound','LKR'=>' Sri Lankan Rupee','LRD'=>' Liberian Dollar','LSL'=>' Lesotho Loti','LTL'=>' Lithuanian Litas','LVL'=>' Latvian Lats','MAD'=>' Moroccan Dirham','MDL'=>' Moldovan Leu','MGA'=>' Malagasy Ariary','MKD'=>' Macedonian Denar','MNT'=>' Mongolian Tögrög','MOP'=>' Macanese Pataca','MRO'=>' Mauritanian Ouguiya','MUR'=>' Mauritian Rupee*','MVR'=>' Maldivian Rufiyaa','MWK'=>' Malawian Kwacha','MXN'=>' Mexican Peso*','MYR'=>' Malaysian Ringgit','MZN'=>' Mozambican Metical','NAD'=>' Namibian Dollar','NGN'=>' Nigerian Naira','NIO'=>' Nicaraguan Córdoba*','NOK'=>' Norwegian Krone','NPR'=>' Nepalese Rupee','NZD'=>' New Zealand Dollar','PAB'=>' Panamanian Balboa*','PEN'=>' Peruvian Nuevo Sol*','PGK'=>' Papua New Guinean Kina','PHP'=>' Philippine Peso','PKR'=>' Pakistani Rupee','PLN'=>' Polish Złoty','PYG'=>' Paraguayan Guaraní*','QAR'=>' Qatari Riyal','RON'=>' Romanian Leu','RSD'=>' Serbian Dinar','RUB'=>' Russian Ruble','RWF'=>' Rwandan Franc','SAR'=>' Saudi Riyal','SBD'=>' Solomon Islands Dollar','SCR'=>' Seychellois Rupee','SEK'=>' Swedish Krona','SGD'=>' Singapore Dollar','SHP'=>' Saint Helenian Pound*','SLL'=>' Sierra Leonean Leone','SOS'=>' Somali Shilling','SRD'=>' Surinamese Dollar*','STD'=>' São Tomé and Príncipe Dobra','SVC'=>' Salvadoran Colón*','SZL'=>' Swazi Lilangeni','THB'=>' Thai Baht','TJS'=>' Tajikistani Somoni','TOP'=>' Tongan Paʻanga','TRY'=>' Turkish Lira','TTD'=>' Trinidad and Tobago Dollar','TWD'=>' New Taiwan Dollar','TZS'=>' Tanzanian Shilling','UAH'=>' Ukrainian Hryvnia','UGX'=>' Ugandan Shilling','UYU'=>' Uruguayan Peso*','UZS'=>' Uzbekistani Som','VND'=>' Vietnamese Đồng','VUV'=>' Vanuatu Vatu','WST'=>' Samoan Tala','XAF'=>' Central African Cfa Franc','XCD'=>' East Caribbean Dollar','XOF'=>' West African Cfa Franc*','XPF'=>' Cfp Franc*','YER'=>' Yemeni Rial','ZAR'=>' South African Rand','ZMW'=>' Zambian Kwacha'),
             'description'  => "<span style='color:red;'>Select the currency in which you like to receive payment the currency that has (*) is unsupported on  American Express Cards.This is independent of store base currency so please update your cart price accordingly.</span>",
        
-       'default' => array( 'USD' ),     
+       'default' => 'USD',     
         ),
 		
 		'stripe_sandbox' => array(
@@ -276,15 +286,7 @@ if(class_exists('WC_Payment_Gateway'))
 							   );
 				die;
          		}
-		
-	
-		if(STRIPE_SANDBOX == 'yes')
-		{ Stripe::setApiKey($this->stripe_testsecretkey);  }
-		else
-		{ Stripe::setApiKey($this->stripe_livesecretkey);   }
-		
-			
-		
+
 	
 		try
 		{
@@ -298,7 +300,7 @@ if(class_exists('WC_Payment_Gateway'))
 						'exp_year' 		=> sanitize_text_field($_POST['stripe_expyear']), 
 						
 						
-						'name'  			=> $wc_order->billing_first_name.'-'.$wc_order->billing_last_name,
+						'name'  			=> $wc_order->billing_first_name.' '.$wc_order->billing_last_name,
 						'address_line1'	=> $wc_order->billing_address_1 ,
 						'address_line2'	=> $wc_order->billing_address_2,
 						'address_city'		=> $wc_order->billing_city,
@@ -320,7 +322,8 @@ if(class_exists('WC_Payment_Gateway'))
 											'Total Tax'      => $wc_order->get_total_tax(),
 											'Total Shipping' => $wc_order->get_total_shipping(),
 											'Customer IP'	  => $this->get_client_ip(),
-											'WP customer #'  => $wc_order->user_id
+											'WP customer #'  => $wc_order->user_id,
+											'Billing Email'  => $wc_order->billing_email,
 										   ) ,
 				'description'  		=> get_bloginfo('blogname').' Order #'.$wc_order->get_order_number(),
 				'shipping' 		     => array(
@@ -383,26 +386,21 @@ if(class_exists('WC_Payment_Gateway'))
 		} // end of function process_payment()
 
 		
-		public function process_refund( $order_id, $amount = null ) {
+		public function process_refund($order_id, $amount = NULL, $reason = '' ) {
 		
 		
 		if($amount > 0 )
 		{
-			
-			if(STRIPE_SANDBOX == 'yes')
-			{ Stripe::setApiKey($this->stripe_testsecretkey);  }
-			else
-			{ Stripe::setApiKey($this->stripe_livesecretkey);   }
-		
 			$CHARGE_ID 	= get_post_meta( $order_id , '_transaction_id', true );
 			$charge 		= Stripe_Charge::retrieve($CHARGE_ID);
 			$refund 		= $charge->refunds->create(
 												array(
-												'amount' 				=> $amount*100,
-												'metadata'			=> array('order_id' => $order_id)
+												'amount' 		=> $amount*100,
+												'metadata'	=> array('Order #' 		=> $order_id,
+																    'Refund reason' => $reason 
+																   ),
 												)
 										      );
-
 			if($refund)	
 			{
 
